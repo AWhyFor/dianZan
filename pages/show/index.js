678e729747fb9ed223ffb81e1f3c08b67a643fee
libo <623807914@qq.com>
2017-01-24 18:15:45 +0800

    add:1.1~9 img show 2.link's css 3.binded data  unresolved:1.comment's css 2.likes' css

--- pages/show/index.js
+++ pages/show/index.js
@@ -2,16 +2,57 @@
 //获取应用实例
 var app = getApp()
 Page({
-  data: {
-    say: 'hi',
-    list: [10, 24, 38, 90]
-  },
-  onLoad: function() {
-    this.setData({
-      say: 'hello'
-    })
-  },
-  getData: function() {
-    var self = this;
-  }
+    data: {
+        name: '船副',
+        avatar: '../../img/me2.jpg',
+        say: 'hiaidoa',
+        bgimg: '../../img/noname2.jpg',
+        lists: [{
+            name: '纳格休斯二大爷',
+            avatar: '../../img/friend2.jpg',
+            text: '测试',
+            imgs: ['../../img/wideImg.jpg','../../img/longImg.jpg','../../img/bigImg.jpg','../../img/smallImg1.jpg','../../img/smallImg3.jpg'],
+            link: {
+                img: '../../img/url.jpg',
+                text: '链接中的文字'
+            },
+            comment: [{
+                name: '王二麻子',
+                to: '大锤',
+                text: '666'
+            }, {
+                name: '路人甲',
+                to: '',
+                text: '233'
+            }],
+            likes: ['王二麻子', '路人甲']
+        },{
+            name: '纳格休斯二大爷',
+            avatar: '../../img/friend2.jpg',
+            text: '测试',
+            imgs: ['../../img/wideImg.jpg','../../img/smallImg2.jpg','../../img/smallImg5.jpg','../../img/smallImg6.jpg'],
+            link: {
+                img: '../../img/url.jpg',
+                text: '链接中的文字'
+            },
+            comment: [{
+                name: '王二麻子',
+                to: '大锤',
+                text: '666'
+            }, {
+                name: '路人甲',
+                to: '',
+                text: '233'
+            }],
+            likes: ['王二麻子', '路人甲']
+        }]
+    },
+    onLoad: function() {
+        this.setData({
+            say: 'hello'
+        })
+    },
+    getData: function() {
+        var self = this;
+    }
 })
